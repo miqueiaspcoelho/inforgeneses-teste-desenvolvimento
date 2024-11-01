8c625b9bec5ce758024803c6f17d8bfb7a28a160
Miquéias Coelho <miqueias.pereira.coelho@gmail.com>
2024-11-01 02:39:24 -0300

    usa bootstrap para estilizar a pagina

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Tarefas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="criar-tarefa.php">Criar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="editar-tarefa.php">Editar</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
