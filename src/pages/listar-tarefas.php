9b2384dd6c2250795b9569f6a16edc0dcfee0656
Miquéias Coelho <miqueias.pereira.coelho@gmail.com>
2024-11-01 05:32:00 -0300

    adiciona ação ao botão excluir

--- src/pages/listar-tarefas.php
+++ src/pages/listar-tarefas.php
@@ -18,7 +18,7 @@
             echo "<td>".$row->descricao."</td>";
             echo "<td>
                     <button onclick= \"location.href='?page=editar&id=".$row->id."';\" class='btn btn-success'>Editar</button>
-                    <button class='btn btn-danger'>Excluir</button>
+                    <button onclick= \"if(confirm('Deseja excluir?')){location.href='?page=gerenciar&acao=excluir&id=".$row->id."';}else{false}\"  class='btn btn-danger'>Excluir</button>
                 </td>";
             echo "</tr>";
         }
